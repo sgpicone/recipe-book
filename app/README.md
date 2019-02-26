11829e8b18f683d0f705f9ac2f308b2aa41b5cb2
Stephen Picone <sgpicone@gmail.com>
2019-02-26 10:41:07 -0500

    Restructured into electron/angular app

--- app/README.md
+++ app/README.md
@@ -1,6 +1,6 @@
-# Recipe Book Frontend
+# Recipe Book
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.1. Angular acts as the frontend, with ElectronJS (4.0.5) acting as the backend. It is built using Electron Forge (5.2.4).
 
 ## Development server
 
@@ -25,3 +25,13 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Electron App
+
+Run `npm run build-electron` to build the angular application and then run the Electron app
+
+Run `npm run forge-start` to run the Electron application
+
+Run `npm run package` to package the app into an executable
+
+Run `npm run make` to build and package the app for the designated target platform
---
+++ app/electronMain.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+//global window reference, or else the window will close automatically when the object is
+//garbage collected
+let win;
+
+// check for presence of app.asar, which is only present in built version of the electron app
+function isDev() {
+    return process.mainModule.filename.indexOf('app.asar') === -1;
+};
+
+function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 600 });
+    console.log(path.join(__dirname, '/dist/recipe-book/index.html'));
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '/dist/recipe-book/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    if (isDev()) {
+        // if in development mode, open dev tools.
+        win.webContents.openDevTools();
+    }   
+    
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + 
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
+});
